a21b7ca37067aa7bc5d789e23b40a91632b91d3c
DanSousa <dansousac2@gmail.com>
2023-07-28 11:59:44 -0300

    created base buttons

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -12,10 +12,34 @@ class Home extends React.Component {
 
     }
 
+    changeBackColor = (button) => {
+        button.classList.add("butonSelected");
+    }
+
+    turnBackGroundDefault = (button) => {
+        button.classList.remove("butonSelected");
+    }
+
     render() {
         return(
-            <div>
-                <b>Hello!</b>
+            <div className='Principal'>
+                <button title='Criar Novo' id='button01' ref={elem => this.buttCollab = elem} 
+                    onMouseEnter={() => this.changeBackColor(this.buttCollab)}
+                    onMouseLeave={() => this.turnBackGroundDefault(this.buttCollab)}
+                    >Criar Colaborador
+                </button>
+                <br/>
+                <button title='Criar Novo' id='button01' ref={elem => this.buttItem = elem} 
+                    onMouseEnter={() => this.changeBackColor(this.buttItem)}
+                    onMouseLeave={() => this.turnBackGroundDefault(this.buttItem)}
+                    >Criar Item
+                </button>
+                <br/>
+                <button title='Criar Novo' id='button01' ref={elem => this.buttCoffee = elem} 
+                    onMouseEnter={() => this.changeBackColor(this.buttCoffee)}
+                    onMouseLeave={() => this.turnBackGroundDefault(this.buttCoffee)}
+                    >Listar Cafés da Manhã
+                </button>
             </div>
         )
     }
